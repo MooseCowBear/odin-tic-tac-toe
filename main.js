23a200ab118e27463fc1410dcfa7824e2dd8a2d8
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-10 14:00:14 -0400

    debug

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const ComputerPlayer = (name, mark) => {
   const computer = Player(name, mark);
   const makeMove = (board) => {
     //grab any random empty square for now
-    const possibleMoves = board.board.map((elem, i) => (elem) === "" ? i : -1).filter(elem => elem !== -1);
+    const possibleMoves = board.map((elem, i) => (elem) === "" ? i : -1).filter(elem => elem !== -1);
     const index = Math.floor(Math.random() * possibleMoves.length);
     return possibleMoves[index];
   };
@@ -55,18 +55,25 @@ const gameController = (playerName1, playerName2 = null) => {
 
   const markOne = coinFlip ? "X" : "0";
   const markTwo = coinFlip ? "0" : "X";
+
   const player1 = Player(playerName1, markOne);
-  const player2 = ComputerPlayer("Hal", markTwo);
+  let player2 = ComputerPlayer("Hal", markTwo);
+
+  let hasComputerPlayer = true;
 
   if (playerName2) {
     player2 = Player(playerName2, markTwo);
+    hasComputerPlayer = false;
   }
 
   const board = Board();
 
   let winner = null;
 
-  let currPlayer = player1.mark === "X" ? player1 : player2; 
+  let currPlayer = player1.getMark() === "X" ? player1 : player2; 
+
+  console.log("player1", player1, "player2", player2, "curr player", currPlayer);
+  console.log(player1.getMark(), player2.getMark());
 
   const getBoard = () => {
     return board.board;
@@ -87,7 +94,7 @@ const gameController = (playerName1, playerName2 = null) => {
   const takeTurn = (index) => {
     const player = getCurrPlayer();
 
-    board.updateBoard(index, player.mark); 
+    board.updateBoard(index, player.getMark()); 
     _updatePlayer();
 
     //check for winners
@@ -96,11 +103,14 @@ const gameController = (playerName1, playerName2 = null) => {
 
   const _gameFinished = (player) => {
     //want to check if mark is winning mark 
-    const indices = board.board.map((elem, i) => (elem) === player.mark ? i : -1).filter(elem => elem !== -1);
+    const indices = board.board.map((elem, i) => (elem) === player.getMark() ? i : -1).filter(elem => elem !== -1);
+    
     let won = false;
     for (let i = 0; i < winConditions.length; i ++) {
       won = winConditions[i].every(v => indices.includes(v));
       if (won) {
+        console.log("a winner");
+
         winner = player; //not ideal to do this here....
         break;
       }
@@ -109,12 +119,7 @@ const gameController = (playerName1, playerName2 = null) => {
   }
 
   const computerPlayer = () => {
-    if (playerName2 === null) {
-      return null;
-    }
-    else {
-      return player2;
-    }
+    return hasComputerPlayer ? player2 : null;
   }
 
   return {
@@ -131,6 +136,8 @@ const gameController = (playerName1, playerName2 = null) => {
 
 //want to call this when "player form" has been submitted
 const screenController = (one, two) => {
+  console.log("one", one, "two,", two);
+
   const game = gameController(one, two);
   const computer = game.computerPlayer();
 
@@ -138,11 +145,14 @@ const screenController = (one, two) => {
 
   //cache dom
   const boardDiv = document.getElementById("board");
-  const annouceDiv = document.getElementById("announcement");
+  const annouceDiv = document.getElementById("annoucement");
+
+  console.log(boardDiv, annouceDiv);
 
   const updateScreen = () => {
     const currPlayer = game.getCurrPlayer();
     const board = game.getBoard();
+    console.log("the current board is", board);
 
     //update annoucement
     if (gameOver) {
@@ -150,8 +160,10 @@ const screenController = (one, two) => {
       annouceDiv.textContent = "The game is finished";
     }
     else {
-      annouceDiv.textContent = `${currPlayer.name}'s turn.`
+      annouceDiv.textContent = `${currPlayer.getName()}'s turn.`
     }
+
+    boardDiv.textContent = "";
 
     //update board
     for(let i = 0; i < board.length; i ++){
@@ -160,7 +172,7 @@ const screenController = (one, two) => {
       cellButton.dataset.index = i;
       cellButton.textContent = elem;
 
-      if (elem === "" || gameOver) { 
+      if (elem !== "" || gameOver) { 
         cellButton.disabled = true;
       }
       boardDiv.appendChild(cellButton);
@@ -169,16 +181,20 @@ const screenController = (one, two) => {
 
   const clickHandler = (e) => {
     //might need a check for is the curr player human. 
+    console.log("in click handler");
+
     const currPlayer = game.getCurrPlayer();
 
     if (e.target.tagName.toLowerCase() === "button" && currPlayer.isHuman()) {
+      console.log("taking a human turn");
+
       gameOver = game.takeTurn(e.target.dataset.index);
       updateScreen();
 
       if (computer) {
         console.log("computer takes a turn");
 
-        const computerTurn = computer.makeMove();
+        const computerTurn = computer.makeMove(game.getBoard());
         gameOver = game.takeTurn(computerTurn);
         updateScreen();
       }
@@ -200,7 +216,7 @@ const screenController = (one, two) => {
   if (computer) {
     console.log("computer takes first turn");
 
-    const computerTurn = computer.makeMove();
+    const computerTurn = computer.makeMove(game.getBoard()); //NOT DEFINED
     gameOver = game.takeTurn(computerTurn);
     updateScreen();
   }
@@ -212,12 +228,25 @@ newGame.addEventListener("click", (e) => {
   //need to check that at least one name has been entered
   e.preventDefault();
   const inputs = document.querySelectorAll('input[type="text"]');
-  if (!inputs.some((elem) => elem !== "")) {
+  const names = [];
+  const error = document.querySelector(".error");
+
+  inputs.forEach((elem) => {
+    names.push(elem.value);
+  });
+  console.log(names);
+
+  if (!names.some((elem) => elem !== "")) {
     //need to show errors and other wise do nothing
+    error.classList.add("show");
   }
   else {
-    screenController(inputs[0], inputs.length > 1 ? inputs[1] : null);
+    screenController(names[0], (names.length > 1 ? names[1] : null));
 
     //plus reset the form and hide it, which should happen first
+    inputs.forEach((elem) => {
+      elem.value = "";
+    });
+    error.classList.remove("show");
   }
 });
