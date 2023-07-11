30ff3ed77d8b1549378943b1d3a308ebdd7fcf7b
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-11 11:15:44 -0400

    add smart computer move

--- main.js
+++ main.js
@@ -13,18 +13,72 @@ const Player = (name, mark) => {
 
 const ComputerPlayer = (name, mark) => {
   const computer = Player(name, mark);
+
   const makeMove = (board) => {
     //grab any random empty square for now
     const possibleMoves = board.map((elem, i) => (elem) === "" ? i : -1).filter(elem => elem !== -1);
     const index = Math.floor(Math.random() * possibleMoves.length);
     return possibleMoves[index];
   };
+
+  const makeSmartMove = (board, winConditions) => {
+    const moveScores = _startingMoveScores();
+    const opponentMark = mark === "X" ? "O" : "X";
+
+    for(let i = 0; i < board.length; i ++) {
+      if (board[i] !== "") {
+        moveScores.set(i, 0); //unavailable moves have no value
+      }
+      else {
+        const wins = winConditions.get(i);
+        for (const w of wins) {
+          if (_checkMove(board, w, mark)) {
+            //a winning move -- doesn't have to be
+            // + infinity, just any number that guarantees it 
+            //has the highest score.
+            moveScores.set(i, moveScores.get(i) + Infinity);
+            break; //can't get a better move
+          }
+          else if (_checkMove(board, w, opponentMark)) {
+            //a forced move -- doesn't matter what we add here.
+            //as long as its finite and it will guarantee
+            //that a forced move gets a higher score than any 
+            //non-forced move.
+            moveScores.set(i, moveScores.get(i) + 10);
+          }
+        }
+      }
+    }
+    //return the move with the highest score
+    return [...moveScores.keys()].reduce((a, e) => moveScores.get(e) > moveScores.get(a) ? e : a);
+  };
+
+  const _checkMove = (board, winCondition, m) => {
+    //helper method to check whether move is forced or winning
+    const [p, q] = winCondition;
+    
+    if (board[p] === m && board[q] === m) {
+      return true;
+    }
+    return false;
+  };
+
+  const _startingMoveScores = () => {
+    return new Map([
+                    [1, 2], [3, 2], [5, 2], [7, 2],
+                    [0, 3], [2, 3], [6, 3], [8, 3],
+                    [4, 4]
+                  ]);
+  };
+  
   const isHuman = () => {
     return false;
   };
+
   return {
     ...computer,
     makeMove,
+    makeSmartMove,
     isHuman
   }
 };
@@ -36,25 +90,31 @@ const Board = () => {
   const updateBoard = (index, mark) => {
     board[index] = mark;
   };
-  return {board, updateBoard};
+
+   const getSquare = (index) => {
+     return board[index];
+   };
+
+  return {board, updateBoard, getSquare};
 };
 
 const gameController = (playerName1, playerName2 = null) => {
-  const winConditions = [
-                          [0, 1, 2], 
-                          [3, 4, 5], 
-                          [6, 7, 8],
-                          [0, 3, 6],
-                          [1, 4, 7],
-                          [2, 5, 8],
-                          [0, 4, 8],
-                          [2, 4, 6]
-                        ];
+  const winConditions = new Map([
+                      [0, [[1, 2], [3, 6], [4, 8]]],
+                      [1, [[0, 2], [4, 7]]],
+                      [2, [[0, 1], [5, 8], [4, 6]]],
+                      [3, [[4, 5], [0, 6]]],
+                      [4, [[3, 5], [1, 7], [0, 8], [2, 6]]],
+                      [5, [[3, 4], [2, 8]]],
+                      [6, [[7, 8], [0, 3], [2, 4]]],
+                      [7, [[6, 8], [1, 4]]],
+                      [8, [[6, 7], [2, 5], [0,4]]]
+                    ]);
 
   const coinFlip = Math.floor(Math.random() * 2);
 
-  const markOne = coinFlip ? "X" : "0";
-  const markTwo = coinFlip ? "0" : "X";
+  const markOne = coinFlip ? "X" : "O";
+  const markTwo = coinFlip ? "O" : "X";
 
   const player1 = Player(playerName1, markOne);
   let player2 = ComputerPlayer("Hal", markTwo);
@@ -95,48 +155,51 @@ const gameController = (playerName1, playerName2 = null) => {
     _updatePlayer();
 
     //check for winners
-    return _gameFinished(player);
-  }
+    return _gameFinished(player, index);
+  };
 
-  const _gameFinished = (player) => {
-    //want to check if mark is winning mark 
-    const indices = board.board.map((elem, i) => (elem) === player.getMark() ? i : -1).filter(elem => elem !== -1);
-    
+  const _gameFinished = (player, index) => {
+    const possibleWins = winConditions.get(parseInt(index));
     let won = false;
-    for (let i = 0; i < winConditions.length; i ++) {
-      won = winConditions[i].every(v => indices.includes(v));
-      if (won) {
-        winner = player; //not ideal to do this here....
+
+    for (const p of possibleWins) {
+      if (board.getSquare(p[0]) === board.getSquare(p[1]) && board.getSquare(p[0]) === player.getMark()) {
+        won = true;
+        winner = player;
         break;
       }
     }
     return won || !board.board.includes("");
-  }
+  };
 
   const computerPlayer = () => {
     return hasComputerPlayer ? player2 : null;
-  }
+  };
+
+  const getWinConditions = () => {
+    //computer player needs this
+    return winConditions;
+  };
 
   return {
     getCurrPlayer,
     takeTurn,
     getBoard,
     getWinner,
-    computerPlayer
+    computerPlayer,
+    getWinConditions
   }
 };
 
-//for screen controller - pass bool to updateScreen() that denotes end of game. redraws but accounces
-//winner or draw instead of player's turn.
-
-//want to call this when "player form" has been submitted
+//called when "player form" has been submitted
 const screenController = (one, two) => {
   const game = gameController(one, two);
   const computer = game.computerPlayer();
+  const winConditions = game.getWinConditions();
 
   let gameOver = false;
 
-  //cache dom
+  //cache dom elements
   const boardDiv = document.getElementById("board");
   const annouceDiv = document.getElementById("annoucement");
 
@@ -146,8 +209,8 @@ const screenController = (one, two) => {
 
     //update annoucement
     if (gameOver) {
-      //want to annoucewinner, else draw -- NEEDS TO BE FINISHED
       _annouceResult();
+      boardDiv.removeEventListener("click", clickHandler);
     }
     else {
       annouceDiv.textContent = `${currPlayer.getName()}'s turn.`
@@ -155,7 +218,7 @@ const screenController = (one, two) => {
 
     boardDiv.textContent = "";
 
-    //update board
+    //redraw board
     for(let i = 0; i < board.length; i ++){
       const elem = board[i];
       const cellButton = document.createElement("button");
@@ -179,10 +242,10 @@ const screenController = (one, two) => {
 
       if (computer) {
         setTimeout(() => {
-          const computerTurn = computer.makeMove(game.getBoard()); 
+          const computerTurn = computer.makeSmartMove(game.getBoard(), winConditions); 
           gameOver = game.takeTurn(computerTurn); 
           updateScreen();
-        }, 500);
+        }, 1000);
       }
     }
   }
@@ -190,22 +253,24 @@ const screenController = (one, two) => {
   const _annouceResult = () => {
     const winner = game.getWinner();
     if (winner) {
-      annouceDiv.textContent = `The winner is ${winner.getName()}.`;
+      annouceDiv.textContent = `${winner.getName()} wins!`;
     }
     else {
       annouceDiv.textContent = "It is a draw.";
     }
+    annouceDiv.classList.add("gameover");
   }
 
   boardDiv.addEventListener("click", clickHandler);
   updateScreen();
 
+  //is the computer the first player? if so, it needs to move first
   if (computer && computer.getMark() === "X") {
     setTimeout(() => {
-      const computerTurn = computer.makeMove(game.getBoard()); 
+      const computerTurn = computer.makeSmartMove(game.getBoard(), winConditions); 
       gameOver = game.takeTurn(computerTurn); 
       updateScreen();
-    }, 500);
+    }, 1000);
   }
 };
 
@@ -218,6 +283,7 @@ const screenController = (one, two) => {
 
   showFormBtn.addEventListener("click", () => {
     playerForm.classList.add("show");
+    resetBoard();
   });
 
   startNewGame.addEventListener("click", (e) => {
@@ -250,5 +316,14 @@ const screenController = (one, two) => {
     });
     error.classList.remove("show");
     playerForm.classList.remove("show");
+  }
+
+  function resetBoard() {
+    const annouceDiv = document.getElementById("annoucement");
+    const boardDiv = document.getElementById("board");
+
+    annouceDiv.textContent = ""; //clear out any message from prev game
+    annouceDiv.classList.remove("gameover");
+    boardDiv.textContent = ""; //reset the board
   }
 })();
